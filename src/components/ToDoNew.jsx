07252a9728392957353144210086badc09c96260
Đặng Đình Vũ <dangdinhvu221@gmail.com>
2025-12-03 10:38:35 +0700

    Re-render and State

--- src/components/ToDoNew.jsx
+++ src/components/ToDoNew.jsx
@@ -1,9 +1,24 @@
+import { useState } from 'react';
+
 const TodoNew = (props) => {
-    const {addNewTodo} = props;
-    // addNewTodo("Learn ReactJS");
+    const { addNewTodo } = props;
+    const [text, setText] = useState('');
+
+    const handleAdd = () => {
+      if (!text.trim()) return;
+      addNewTodo(text.trim());
+      setText('');
+    }
+
   return (
-    <div>TodoNew
-        {console.log(props)}
+    <div className="todo-new">
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Add new todo"
+      />
+      <button onClick={handleAdd}>Add</button>
+      <span style={{ marginLeft: 8 }}> My text input is = {text}</span>
     </div>
   )
 }
--- src/components/TodoData.jsx
+++ src/components/TodoData.jsx
@@ -1,35 +1,14 @@
 // props là 1 object chứa tất cả các thuộc tính được truyền từ component cha xuống
-const TodoData = ({fullname,age,data}) => {
-// props có thể được truyền trực tiếp hoặc destructuring
-// có những cách lấy props như sau:
-// Cách 1: Lấy trực tiếp từ đối tượng props
-// const TodoData = (prop) => {
-// Cách 2: Destructuring trong tham số hàm
-// const TodoData = ({fullname, age, data}) => {
-// Cách 3: Destructuring trong thân hàm
-// const {fullname, age, data} = prop;
+const TodoData = ({ fullname, todos = [] }) => {
   return (
     <div className="todo-data">
-        {/* <div>
-            name: {prop.fullname} <br/>
-            age: {prop.age} <br/>
-            address: {prop.data.address} <br/>
-            job: {prop.data.job} <br/>
-        </div> */}
-        {/* <div>
-            name: {fullname} <br/>
-            age: {age} <br/>
-            address: {data.address} <br/>
-            job: {JSON.stringify(data)} <br/>
-        </div> */}
-        <div>
-            name: {fullname} <br/>
-            age: {age} <br/>
-            address: {data.address} <br/>
-            job: {JSON.stringify(data)} <br/>
-        </div>
-        <div>To do Data</div>
+        <div style={{ marginBottom: 8 }}>My name is {fullname}</div>
+        {todos.map(item => (
+          <div key={item.id}>{item.name}</div>
+        ))}
+        <div style={{ marginTop: 8 }}>{JSON.stringify(todos)}</div>
     </div>
   )
-}   
+}
+  
 export default TodoData;
