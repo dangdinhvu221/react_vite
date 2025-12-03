b974e20d1f4730a2663e01f5b2bbf6424ffbd736
Đặng Đình Vũ <dangdinhvu221@gmail.com>
2025-12-03 08:41:31 +0700

    Component?

---
+++ src/components/lean/MyComponent.jsx
@@ -0,0 +1,11 @@
+// create MyComponent 
+  const MyComponent = () =>{
+    console.log(">>>>>>>> run my MyComponent");
+    return (
+      <div>
+        eric & chơi bời react.!!
+      </div>
+    );
+  } 
+
+  export default MyComponent;
---
+++ src/components/lean/SecondComponent.jsx
@@ -0,0 +1,18 @@
+const SecondComponent = () => {
+    return (
+        <div>
+            Đây là Second Component
+        </div>
+    )
+}
+
+const ThirdComponent = () => {
+    return (
+        <div>
+            Đây là Third Component
+        </div>
+    )
+}
+
+export {SecondComponent, ThirdComponent};
+
